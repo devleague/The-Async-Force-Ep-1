39a0e7fd9c126a975a13593beeefd809e84b7274
Chris Min <chris.min216@gmail.com>
2017-09-22 16:24:23 -1000

    attempting to create separate URL subroutine for nested problem

--- js/app.js
+++ js/app.js
@@ -9,14 +9,14 @@ var e = document.getElementById("filmList");
 function requestProcess(request, url, elem) { 
   var reqListener = function() {
     let newHTML = JSON.parse(this.responseText); //the parse is important.
-    elem.innerHTML = newHTML.name;
+    if (elem) { //only if an elem is passed in
+      elem.innerHTML = newHTML.name;
+    }
   }
   request.addEventListener('load', reqListener);
   request.open("GET", url, true);
   request.send();
 }
-
-console.log();
 
 var oReq = new XMLHttpRequest();
 requestProcess(oReq, 'http://swapi.co/api/people/4/', a);
@@ -26,10 +26,13 @@ requestProcess(pReq, 'http://swapi.co/api/planets/1/', b);
 
 var qReq = new XMLHttpRequest();
 requestProcess(qReq, 'http://swapi.co/api/people/14/', c);
-console.log(qReq);
 
 var rReq = new XMLHttpRequest();
 requestProcess(rReq, 'http://swapi.co/api/species/1/', d);
+
+var sReq = new XMLHttpRequest();
+requestProcess(sReq, 'http://swapi.co/api/films/');
+console.log(sReq);
 
 
 
